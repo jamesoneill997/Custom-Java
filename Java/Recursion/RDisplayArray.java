297211b4bd02046b10ef5bf101ac622f09904b61
James O'Neill <36668255+jamesoneill997@users.noreply.github.com>
2019-02-07 01:14:15 +0000

    Add files via upload

--- Java/Recursion/RDisplayArray.java
+++ Java/Recursion/RDisplayArray.java
@@ -2,23 +2,23 @@ import java.util.*;
 
 public class RDisplayArray
 {
-	static int display(List<Integer> a)
+	static String display(List<Integer> a)
 	{
-		if(a.size() == 0)
-		{	
-			return 0;
+		if(a.size()==0)
+		{
+			return "";
 		}
 
 		if(a.size() == 1)
 		{
-			return 1;
+			return Integer.toString(a.get(0));
 		}
 
 		else
 		{
-			return 1 + display(a.subList(1,a.size()));
+			return Integer.toString(a.get(0)) + " " + display(a.subList(1,a.size()));
 		}
-
 
 	}
 }
+
