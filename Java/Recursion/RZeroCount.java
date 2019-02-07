291479faec38718b575830535481a75d2c0f6afe
James O'Neill <36668255+jamesoneill997@users.noreply.github.com>
2019-02-07 09:39:20 +0000

    Add files via upload

---
+++ Java/Recursion/RZeroCount.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class RZeroCount
+{
+	static int total(List<Integer> nums)
+	{
+		if(nums.size() == 0)
+		{
+			return 0;
+		}
+
+		else
+		{
+			if(nums.get(0) == 0)
+			{
+				return 1 + total(nums.subList(1,nums.size()));
+			}	
+		}
+	return 1;
+	}
+}
